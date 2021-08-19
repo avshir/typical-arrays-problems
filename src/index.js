21c8bb6f8c0edf5a2273f806d6b95dd12d7990e5
avshir <av.shirinskaya@gmail.com>
2021-08-19 17:23:51 +0300

    feat: add solution, test 100%

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array = []) {
+	if (array.length === 0) {
+		//console.log(array.length);
+		return 0;
+	} else { 
+		array.sort((a, b) => a - b);
+		return array[0];
+	}
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = []) {
+	if (array.length === 0) {
+		return 0;
+	} else { 
+		array.sort((a, b) => a - b);
+		return array[array.length - 1];
+	}
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+	if (array.length === 0) {
+		return 0;
+	}
+	else {
+		let res = 0;
+	// 	for (let key of array) {
+	// 		res += key;
+	// 	}
+	// res = res / array.length;
+		
+		res = array.reduce((accumulator, currentValue) => accumulator + currentValue) / array.length;
+		return res;
+	}
 }
